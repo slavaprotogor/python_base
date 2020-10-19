0859359c6bbfc51ba0c596a1a66a2629af7452ab
zanin <zanin.vyacheslav@220-volt.ru>
2020-10-19 23:13:18 +0300

    add version 2 for task2

--- homeworks/lesson2/task2.py
+++ homeworks/lesson2/task2.py
@@ -4,7 +4,25 @@
 При нечетном количестве элементов последний сохранить на своем месте.
 Для заполнения списка элементов необходимо использовать функцию input().
 """
+# version 1
+elements = []
+while True:
+    element = input('Введите элемент (q - выход): ')
+    if element == 'q':
+        break
+    elements.append(element)
 
+elements_len = len(elements)
+index = 1
+while index < elements_len:
+    element = elements[index]
+    elements[index] = elements[index - 1]
+    elements[index - 1] = element
+    index += 2
+
+print('Результат: ', elements)
+
+# version 2
 elements = []
 while True:
     element = input('Введите элемент (q - выход): ')
