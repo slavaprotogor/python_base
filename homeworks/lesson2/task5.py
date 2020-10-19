4f8f0c878f512394044cc143810a29c79bee67bd
zanin <zanin.vyacheslav@220-volt.ru>
2020-10-19 22:42:58 +0300

    add task 5 and task 6

---
+++ homeworks/lesson2/task5.py
@@ -0,0 +1,32 @@
+"""
+Реализовать структуру «Рейтинг», представляющую собой не возрастающий набор натуральных чисел.
+У пользователя необходимо запрашивать новый элемент рейтинга.
+Если в рейтинге существуют элементы с одинаковыми значениями,
+то новый элемент с тем же значением должен разместиться после них.
+"""
+
+digits = []
+while True:
+    digit = input('Введите число (q - выход): ')
+
+    if digit == 'q':
+        break
+
+    if not digit.isdigit():
+        print('Должно быть введино целое число. Попробуйте еще раз.')
+        continue
+
+    digit = int(digit)
+    digits_len = len(digits)
+    for index, digit_item in enumerate(digits):
+        if digit > digit_item:
+            digits.insert(index, digit)
+            break
+
+        if index == digits_len - 1:
+            digits.append(digit)
+            break
+    else:
+        digits.append(digit)
+
+print('Неубывающая последовательность: ', digits)
