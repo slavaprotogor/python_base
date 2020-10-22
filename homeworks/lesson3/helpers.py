86312f9f79acded0860868177b9955b8d01b8af2
Vyacheslav Zanin <zanin.vyacheslav@220-volt.ru>
2020-10-22 17:10:55 +0300

    add quick_sort and my_pow

--- homeworks/lesson3/helpers.py
+++ homeworks/lesson3/helpers.py
@@ -18,15 +18,54 @@ def input_type(message: str, check_type) -> int:
             print('Попробуйте еще раз')
 
 
-def my_sorted(items: list, reverse: bool = False) -> list:
+def my_sorted(elements: list, reverse: bool = False) -> list:
     """
-    Функция возвращает отсортированный массив
+    Функция возвращает отсортированный массив.
+    Используется алгоритм "Быстрая сортировка".
 
-    :param items: список элементов
+    :param elements: список элементов
     :param reverse: сортровать в обратном порядке
     :return: отсортированный список
     """
-    return sorted(items, reverse=True)
+
+    def partition(nums, low, high):
+        # Выбираем средний элемент в качестве опорного
+        # Также возможен выбор первого, последнего
+        # или произвольного элементов в качестве опорного
+        pivot = nums[(low + high) // 2]
+        i = low - 1
+        j = high + 1
+        while True:
+            i += 1
+            while reverse and nums[i] > pivot or \
+                    not reverse and nums[i] < pivot:
+                i += 1
+
+            j -= 1
+            while reverse and nums[j] < pivot or \
+                    not reverse and nums[j] > pivot:
+                j -= 1
+
+            if i >= j:
+                return j
+
+            # Если элемент с индексом i (слева от опорного) больше, чем
+            # элемент с индексом j (справа от опорного), меняем их местами
+            nums[i], nums[j] = nums[j], nums[i]
+
+    def quick_sort(nums):
+        # Создадим вспомогательную функцию, которая вызывается рекурсивно
+        def _quick_sort(items, low, high):
+            if low < high:
+                # This is the index after the pivot, where our lists are split
+                split_index = partition(items, low, high)
+                _quick_sort(items, low, split_index)
+                _quick_sort(items, split_index + 1, high)
+
+        _quick_sort(nums, 0, len(nums) - 1)
+
+    quick_sort(elements)
+    return elements
 
 
 def my_sum(items: list) -> float:
@@ -50,7 +89,15 @@ def my_pow(base: float, exp: float) -> float:
     :param exp: степень
     :return: возведенное в степень число
     """
-    return pow(base, exp)
+    if exp == 0:
+        return 1
+    if exp == -1:
+        return 1. / base
+    result = my_pow(base, exp // 2)
+    result *= result
+    if exp % 2:
+        result *= base
+    return result
 
 
 def my_map(func: callable, items: list) -> list:
