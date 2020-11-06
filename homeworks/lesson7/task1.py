f89b4fcba9c324dcdd66090a6e737154f4d60969
Vyacheslav Zanin <zanin.vyacheslav@220-volt.ru>
2020-11-06 10:33:34 +0300

    add task1, task2 and task3

---
+++ homeworks/lesson7/task1.py
@@ -0,0 +1,86 @@
+"""
+Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод __init__()),
+который должен принимать данные (список списков) для формирования матрицы.
+Подсказка: матрица — система некоторых математических величин, расположенных в виде прямоугольной схемы.
+Примеры матриц вы найдете в методичке.
+Следующий шаг — реализовать перегрузку метода __str__() для вывода матрицы в привычном виде.
+Далее реализовать перегрузку метода __add__() для реализации операции сложения двух объектов класса
+Matrix (двух матриц).
+Результатом сложения должна быть новая матрица.
+"""
+from typing import List
+
+
+class Matrix:
+    """ Класс матрицы """
+    def __init__(self, items: List[List[float or int]]):
+        """ Инициализация инстанса класса
+
+        :param items: список списков с числами
+        """
+        if not isinstance(items, list) or not isinstance(items[0], list):
+            raise ValueError('Items must be a list of list')
+
+        self.__items = items
+
+    @property
+    def row_count(self) -> int:
+        """ Свойство возвращает количество строк матрицы
+
+        :return: количество строк
+        """
+        return len(self.__items)
+
+    @property
+    def column_count(self) -> int:
+        """ Свойство возвращает количество столбцов матрицы
+
+        :return: количество строк
+        """
+        return len(self.__items[0])
+
+    @property
+    def items(self) -> List[List[float or int]]:
+        """ Свойство возвращает список списков
+
+        :return: список списков
+        """
+        return self.__items
+
+    def __str__(self) -> str:
+        """ Строуовое представление матрицы """
+        values_str = ''
+        for row in self.__items:
+            values_str += ', '.join(map(str, row)) + '\n'
+        return values_str
+
+    def __add__(self, other):
+        """ Сложение матрицы
+
+        :param other:
+        :return:
+        """
+        if not isinstance(other, Matrix):
+            raise ValueError('The object must be a Matrix')
+
+        items_new = []
+        for row in range(self.row_count):
+            row_new = [] 
+            for column in range(self.column_count):
+                row_new.append(self.items[row][column] + other.items[row][column])
+            items_new.append(row_new)
+
+        return Matrix(items_new)
+
+
+if __name__ == '__main__':
+    m1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print('Matrix 1:')
+    print(m1)
+
+    m2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print('Matrix 2:')
+    print(m2)
+
+    print('Matrix sum:')
+    print(m1 + m2)
---
+++ homeworks/lesson7/task3.py
@@ -0,0 +1,124 @@
+"""
+Реализовать программу работы с органическими клетками. Необходимо создать класс Клетка.
+В его конструкторе инициализировать параметр, соответствующий количеству клеток (целое число).
+В классе должны быть реализованы методы перегрузки арифметических операторов: сложение (__add__()),
+вычитание (__sub__()), умножение (__mul__()), деление (__truediv__()).
+Данные методы должны применяться только к клеткам и выполнять увеличение, уменьшение, умножение и обычное
+(не целочисленное) деление клеток, соответственно. В методе деления должно осуществляться округление значения до целого числа.
+Сложение. Объединение двух клеток. При этом число ячеек общей клетки должно равняться сумме ячеек исходных двух клеток.
+Вычитание. Участвуют две клетки. Операцию необходимо выполнять только если разность количества ячеек двух клеток больше
+ нуля, иначе выводить соответствующее сообщение.
+Умножение. Создается общая клетка из двух.
+Число ячеек общей клетки определяется как произведение количества ячеек этих двух клеток.
+Деление. Создается общая клетка из двух. Число ячеек общей клетки определяется как целочисленное деление количества ячеек этих двух клеток.
+В классе необходимо реализовать метод make_order(), принимающий экземпляр класса и количество ячеек в ряду.
+Данный метод позволяет организовать ячейки по рядам.
+Метод должен возвращать строку вида *****\n*****\n*****..., где количество ячеек между \n равно переданному аргументу.
+Если ячеек на формирование ряда не хватает, то в последний ряд записываются все оставшиеся.
+Например, количество ячеек клетки равняется 12, количество ячеек в ряду — 5.
+Тогда метод make_order() вернет строку: *****\n*****\n**.
+Или, количество ячеек клетки равняется 15, количество ячеек в ряду — 5.
+Тогда метод make_order() вернет строку: *****\n*****\n*****.
+"""
+
+
+class Cell:
+    """ Класс клеткаи """
+    def __init__(self, cores: int):
+        """ Инициализация инстанса класса
+
+        :param cores: количество ячеек
+        """
+        if not isinstance(cores, int) or cores <= 0:
+            raise ValueError('Cores must be a positive integer')
+
+        self.__cores = cores
+
+    @property
+    def number(self):
+        """ Свойство возвращает количество ячеек
+
+        :return: кол-во ячеек
+        """
+        return self.__cores
+
+    def __add__(self, other):
+        """ Сложение клеток и получение новой клетки
+
+        :param other: клетка, объект типа Cell
+        :return: новая клетка
+        """
+        if not isinstance(other, Cell):
+            raise ValueError('The object must be a Cell')
+
+        return Cell(self.__cores + other.number)
+
+    def __sub__(self, other):
+        """ Вычетание клеток и получение новой клетки
+
+        :param other: клетка, объект типа Cell
+        :return: новая клетка
+        """
+        if not isinstance(other, Cell):
+            raise ValueError('The object must be a Cell')
+        if other.number >= self.__cores:
+            raise ValueError('Cell number more then number of first Cell')
+
+        return Cell(max(self.__cores - other.number, 0))
+
+    def __mul__(self, other):
+        """ Умножение клеток и получение новой клетки
+
+        :param other: клетка, объект типа Cell
+        :return: новая клетка
+        """
+        if not isinstance(other, Cell):
+            raise ValueError('The object must be a Cell')
+
+        return Cell(self.__cores * other.number)
+
+    def __truediv__(self, other):
+        """ Деление клеток и получение новой клетки
+
+        :param other: клетка, объект типа Cell
+        :return: новая клетка
+        """
+        if not isinstance(other, Cell):
+            raise ValueError('The object must be a Cell')
+
+        return Cell(round(self.__cores / other.number))
+
+    def make_order(self, chunk_size: int) -> str:
+        """ Метод рисует строковое представление ячеек
+        клетки разделенных на группы
+
+        :param chunk_size: размер группы
+        :return: строковое представление ячеек
+        """
+        chunks, tail = divmod(self.__cores, chunk_size)
+
+        cells_graph = ['*' * chunk_size for _ in range(chunks)]
+        if tail:
+            cells_graph.append('*' * tail)
+        return '/n'.join(cells_graph)
+
+
+if __name__ == '__main__':
+    cell1 = Cell(23)
+    cell2 = Cell(5)
+
+    print('Сумма:')
+    cell3 = cell1 + cell2
+    print(cell3.make_order(5))
+
+    print('Вычитание:')
+    cell3 = cell1 - cell2
+    print(cell3.make_order(5))
+
+    print('Умножение:')
+    cell3 = cell1 * cell2
+    print(cell3.make_order(5))
+
+    print('Деление:')
+    cell3 = cell1 / cell2
+    print(cell3.make_order(5))
